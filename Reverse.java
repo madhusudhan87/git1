f19a34a432f1e08e774d7ead3f43d9dd1b2b6708
anagalakshmi.ip <anagalakshmi.ip@gmail.com>
2018-12-31 00:27:51 -0800

    changed value for second time

--- Reverse.java
+++ Reverse.java
@@ -2,7 +2,7 @@ public class Reverse
 {
 public static void main(String[] madhu)
 {
-int number=6;
+int number=10;
 int fact=1;
 int i=1;
 while(i<=number)
@@ -10,7 +10,7 @@ while(i<=number)
 fact=fact*i;
 i++;
 }
-System.out.println("the factorial num of: "+number+"is"+ fact);
+System.out.println("the factorial num of:  "+number+"is"+ fact);
 }
 }
 
